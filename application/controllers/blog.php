6a1d1379dd10f671936091bc81d72ccb6b43f9d3
Diarmuid Murphy <diarmuid.com@gmail.com>
2016-06-05 16:06:41 -0700

    simple mvc stuff

---
+++ application/controllers/blog.php
@@ -0,0 +1,16 @@
+<?php
+
+class Blog extends CI_Controller {
+	public function index() {
+		$data['title'] = "My Real Title";
+		$data['heading'] = "My Real Heading";
+
+		$this->load->view('blogview', $data);
+	}
+
+	public function comments() {
+		echo 'Look at this!';
+	}
+}
+
+?>
