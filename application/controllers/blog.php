309843e846621a8ec872eb81cc6aa8accf536de0
Diarmuid Murphy <diarmuid.com@gmail.com>
2016-06-05 16:15:58 -0700

    views complete

--- application/controllers/blog.php
+++ application/controllers/blog.php
@@ -2,6 +2,8 @@
 
 class Blog extends CI_Controller {
 	public function index() {
+		$data['todo_list'] = array('Clean House', 'Call Mom', 'Run Errands');
+
 		$data['title'] = "My Real Title";
 		$data['heading'] = "My Real Heading";
 
